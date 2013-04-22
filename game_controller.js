e6b565932e0bafa8888d9e07bbaada5dff4279a4
calamari <g.tavonius@gmail.com>
2013-04-22 23:29:22 +0200

    chat now in a socket namespace

--- game_controller.js
+++ game_controller.js
@@ -2,14 +2,17 @@
 module.exports = function(app) {
   var io = require('socket.io').listen(app);
 
-  io.sockets.on('connection', function (socket) {
-    socket.on('chat.message', function (data) {
-      // pass it on
-      // TODO: only to relevant games
-      console.log('recieved', 'chat.message', data);
-      socket.broadcast.emit('chat.message', data);
+  var chat = io
+    .of('/chat')
+    .on('connection', function (socket) {
+      console.log("SOCKET", socket);
+      socket.on('message', function (data) {
+        // pass it on
+        // TODO: only to relevant games
+        console.log('recieved', 'message', data);
+        socket.broadcast.emit('message', data);
+      });
     });
-  });
 
   return io;
 };
