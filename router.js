4469e2b80f30950eb8d56a6e6ee07781d5526031
calamari <g.tavonius@gmail.com>
2013-04-22 18:17:10 +0200

    initial with server and socket.io

---
+++ router.js
@@ -0,0 +1,25 @@
+var Handlebars = require('handlebars'),
+    fs         = require('fs'),
+    dispatch   = require('dispatch');
+
+// TODO: this could be exportet in an node module:
+var templates = [];
+
+templates.load = function(filename) {
+  return Handlebars.compile(fs.readFileSync(filename).toString());
+};
+
+fs.readdirSync(__dirname + '/views').forEach(function(file) {
+  var filename = __dirname + '/views/' + file,
+      name     = file.replace('.hbs', '');
+  templates[name] = templates.load(filename);
+  fs.watch(filename, function reloadTemplate(event) {
+    templates[name] = templates.load(filename);
+  });
+});
+
+module.exports = dispatch({
+  '/': function(req, res, next) {
+    res.html(templates.index({}));
+  }
+});
---
+++ server.js
@@ -0,0 +1,20 @@
+
+var connect = require('connect'),
+    quip    = require('quip'),
+    http    = require('http'),
+
+    app     = connect()
+      .use(connect.static(__dirname + '/assets'))
+      .use(connect.static(__dirname + '/public'))
+      .use(connect.bodyParser())
+      .use(connect.query())
+      .use(quip())
+      .use(require(__dirname + '/router')),
+
+    server  = http.createServer(app);
+
+
+server.listen(8124);
+require(__dirname + '/game_controller.js')(server);
+
+console.log("server started on http://127.0.0.1:8124");
