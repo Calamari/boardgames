c151ca915bd39c229a1b525c437857dfd215a9af
calamari <g.tavonius@gmail.com>
2013-04-22 23:24:06 +0200

    chatting now possible

--- game_controller.js
+++ game_controller.js
@@ -3,9 +3,11 @@ module.exports = function(app) {
   var io = require('socket.io').listen(app);
 
   io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
+    socket.on('chat.message', function (data) {
+      // pass it on
+      // TODO: only to relevant games
+      console.log('recieved', 'chat.message', data);
+      socket.broadcast.emit('chat.message', data);
     });
   });
 
