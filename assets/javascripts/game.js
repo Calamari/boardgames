df3d0bad75c332cd4450f1f3feafb2967c1f58ef
calamari <g.tavonius@gmail.com>
2013-05-15 22:19:41 +0200

    A bit nicer view of score of players

--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,6 +1,50 @@
 (function(win, doc, CanvasBoard) {
   "use strict";
 
+  var Score = function(element, names) {
+    this._element = $(element);
+    this._names = names;
+    this.width = 200;
+    this._counters = { 1: 0, 2: 0 };
+    this._createHTML();
+  };
+
+  Score.prototype = {
+    _createHTML: function() {
+      var self = this;
+      this._names.forEach(function(name, index) {
+        self._element.append((index === 0 ? name + ' ' : '') + '<span class="' + name + ' player' + index + '"></span>' + (index === 1 ? ' ' + name : ''));
+      });
+    },
+    _count: function(board, size) {
+      var count = { 1: 0, 2: 0},
+          sum   = 0,
+          x, y;
+
+      for (y = size; y--;) {
+        for (x = size; x--;) {
+          if (board[y] && board[y][x]) {
+            ++count[board[y][x]];
+            ++sum;
+          }
+        }
+      }
+      this._counters = count;
+      return sum;
+    },
+    update: function(board, size) {
+      var sum  = this._count(board, size),
+          self = this;
+
+      this._names.forEach(function(name, index) {
+        var count = self._counters[index+1],
+            size  = (sum > 0) ? (count / sum) * self.width : self.width / self._names.length;
+        self._element.find('.' + name).css({ width: size }).html(count);
+      });
+    }
+  };
+
+
   var Game = function(container, config) {
     this._boardSize = config.boardSize;
     // TODO: make this game specific:
@@ -9,13 +53,13 @@
     this._socket = config.socket;
     this._socketeer = new Socketeer(config.socket, config.socketeerId);
     this._logger = config.logger;
+    this._score = new Score(config.score, config.players);
     this.isSpectator = config.isSpectator;
     this.actualPlayer = config.actualPlayer;
     this.thisPlayerNr = config.thisPlayerNr;
     this._gameStarted = config.gameStarted;
     this._initBoard(config.stones || []);
     this._initSocketeer();
-    this._counters = { 1: $('#counter-1'), 2: $('#counter-2') };
   };
 
   Game.prototype = {
@@ -32,6 +76,7 @@
         }
       }
       this._boardEngine.updateBoard(this._board);
+      this._countPieces();
     },
     _initSocketeer: function() {
       var self = this;
@@ -96,18 +141,7 @@
       });
     },
     _countPieces: function() {
-      var count = { 1: 0, 2: 0},
-          x, y;
-
-      for (y = this._boardSize; y--;) {
-        for (x = this._boardSize; x--;) {
-          if (this._board[y] && this._board[y][x]) {
-            ++count[this._board[y][x]];
-          }
-        }
-      }
-      this._counters[1].html(count[1]);
-      this._counters[2].html(count[2]);
+      this._score.update(this._board, this._boardSize);
     },
     _updateGame: function(data) {
       var self = this,
