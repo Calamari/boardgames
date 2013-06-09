cbda51e08a6ef798d089b45b9d345c1a27a016e6
calamari <g.tavonius@gmail.com>
2013-06-09 23:27:48 +0200

    using jQuery 2.0.2 now

--- assets/javascripts/webpage.js
+++ assets/javascripts/webpage.js
@@ -1,6 +1,6 @@
 //= require vendor/socket.io-0.9.11.js
 //= require vendor/moment.js
-//= require vendor/jquery-1.9.1.js
+//= require vendor/jquery-2.0.2.min.js
 //= require vendor/object.extend.js
 //= require chat.js
 //= require behaviors/data_method.js
