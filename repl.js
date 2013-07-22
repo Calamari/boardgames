31e508d3a942cc2c49cf765ccb48a4ea93579572
calamari <g.tavonius@gmail.com>
2013-07-22 10:40:05 +0200

    first version of repl console

---
+++ repl.js
@@ -0,0 +1,24 @@
+/*jslint node: true */
+'use strict';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var router = require(__dirname + '/router'),
+    app    = require(__dirname + '/app')(router, 'mongodb://localhost/boardgames_', {
+      dbPostfix: process.env.NODE_ENV === 'development' ? 'dev' : process.env.NODE_ENV
+    }),
+    repl   = require('repl');
+
+var instance = repl.start({
+  prompt: 'boardgames> ',
+  input: process.stdin,
+  output: process.stdout
+});
+
+instance.context.router = router;
+instance.context.app = app;
+
+instance.on('exit', function () {
+  console.log('Exiting console, goodbye!');
+  process.exit();
+});
