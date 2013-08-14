70da96329710ac97c2fd0e444d6203ee4db8f97a
Calamari <g.tavonius@gmail.com>
2013-08-14 23:26:20 +0200

    wrapped extend method in (function(){}()); wrapper

--- assets/javascripts/utils/extend.js
+++ assets/javascripts/utils/extend.js
@@ -1,6 +1,9 @@
-function extend(subClass, superClass) {
-  var F = function() {};
-  F.prototype = superClass.prototype;
-  subClass.prototype = new F();
-  subClass.prototype.constructor = subClass;
-}
+(function(win) {
+  'use strict';
+  win.extend = function extend(subClass, superClass) {
+    var F = function() {};
+    F.prototype = superClass.prototype;
+    subClass.prototype = new F();
+    subClass.prototype.constructor = subClass;
+  };
+}(window));
