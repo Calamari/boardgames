244b27961f513901f2e66d2b666ede7cf19e8891
Calamari <g.tavonius@gmail.com>
2013-08-14 23:28:15 +0200

    server has to listen to special ip and port 80 on live server

--- server.js
+++ server.js
@@ -6,13 +6,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var router = require(__dirname + '/router'),
     argv   = require('optimist').argv,
     port   = argv.port || 8124,
+    address= argv.address || '127.0.0.1',
     app    = require(__dirname + '/app')(router, 'mongodb://localhost/boardgames_', {
       dbPostfix: process.env.NODE_ENV == 'development' ? 'dev' : process.env.NODE_ENV
     });
 
-app.server.listen(port);
+app.server.listen(port, address);
 // Should Chat also be done via Socketeer?
 require('./chat_controller.js')(app.server, app.io);
 require('./controllers/game_socket_controller.js')(app.socketeer, app);
 
-console.log("server started on http://127.0.0.1:" + port);
+console.log("server started on http://" + address + ":" + port);
