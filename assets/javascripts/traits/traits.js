44e2d1eb90206a9b2887cca3b38e2a00b2aae28c
Calamari <g.tavonius@gmail.com>
2013-08-14 23:25:59 +0200

    Fixed problem with not defined Object.extend in traits

--- assets/javascripts/traits/traits.js
+++ assets/javascripts/traits/traits.js
@@ -1,5 +1,10 @@
 var traits = {
   apply: function(object, traitName) {
-    return Object.extend(object, traits[traitName]);
+    for (var key in traits[traitName]) {
+      if (traits[traitName].hasOwnProperty(key)) {
+        object[key] = traits[traitName][key];
+      }
+    }
+    return object;
   }
 };
