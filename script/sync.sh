9588307b614e401cfaec5111d0a6ad510b315efd
Calamari <g.tavonius@gmail.com>
2014-02-24 15:02:38 +0100

    chore(deploy): Sync script restarts the server

--- script/sync.sh
+++ script/sync.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 rsync -rv --exclude tmp/ --exclude .git/ --exclude db/ --exclude node_modules/ --exclude log/ . calamari@boardgames.jaz-lounge.com:/var/www/boardgames
+ssh boardgames.jaz-lounge.com 'cd /var/www/boardgames && npm install'
+ssh -t boardgames.jaz-lounge.com 'sudo restart boardgames'
