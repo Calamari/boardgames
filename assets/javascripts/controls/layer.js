1f819fbfdc1906d68dd1b66e0ea8ddb1ebda9aeb
Calamari <g.tavonius@gmail.com>
2014-05-18 19:37:06 +0200

    fix(layer): Links in layer will work several times

--- assets/javascripts/controls/layer.js
+++ assets/javascripts/controls/layer.js
@@ -6,31 +6,32 @@
     this.element = $(element);
     this._config = config;
     //this._wrap = this.element.find('.wrap');
-
-    var self = this;
-    this.element
-      .on('click', '.closer, .cancel-link', function(event) {
-        event.preventDefault();
-        self.close();
-      })
-      .on('click', '.help-link', function(event) {
-        event.preventDefault();
-        self.element.toggleClass('is-helping');
-      })
-      .on('click', '.success-link', function(event) {
-        config.onSuccess && config.onSuccess.call(self);
-        event.preventDefault();
-        self.close();
-      });
     this.open();
   };
 
   Layer.prototype = {
     open: function() {
+      var self = this;
       this.element.removeClass('is-closed');
+      this.element
+        .on('click.layer', '.closer, .cancel-link', function(event) {
+          event.preventDefault();
+          self.close();
+        })
+        .on('click.layer', '.help-link', function(event) {
+          event.preventDefault();
+          self.element.toggleClass('is-helping');
+        })
+        .on('click.layer', '.success-link', function(event) {
+          config.onSuccess && config.onSuccess.call(self);
+          event.preventDefault();
+          self.close();
+        });
     },
     close: function() {
-      this.element.addClass('is-closed');
+      this.element
+        .addClass('is-closed')
+        .off('.layer');
     }
   };
 
